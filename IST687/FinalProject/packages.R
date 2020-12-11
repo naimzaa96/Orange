9aeeb663eda078c4afb95007d1dada085c8efb97
Abu Naimzadeh <naimzaa8@gmail.com>
2020-12-10 19:28:42 -0500

    relevant packages created

---
+++ IST687/FinalProject/packages.R
@@ -0,0 +1,22 @@
+# Package needed to install and run
+
+library(RCurl)
+library(jsonlite)
+library(tidyverse)
+library(dplyr)
+
+# Web Apps
+library(shiny)
+library(shinyWidgets)
+
+#Visualizations 
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(usmap)
+
+## Machine Learning 
+library(arules)
+library(arulesViz)
+library(kernlab)
+library(e1071)
